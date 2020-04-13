2cef94bce5e27170eacecbfec1c038536f3fa3df
sharpmanjt <thibeau.jeremy@gmail.com>
2020-04-13 00:24:15 -0400

    pushing lab 9

---
+++ wk13/getusers_1.php
@@ -0,0 +1,48 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "admin2";
+$password = "";
+$db = "db";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+echo "connected successfully";
+
+echo
+
+    "<form method='get'> 
+        <input type='text' name='command'>
+        <input type='submit'>
+    </form>";
+
+echo "<table style='border-collapse: collapse' border='1px solid black'>
+<thead>
+<th>
+<td>Id</td>
+<td>Username</td>
+<td>Email</td>
+<td>First Name</td>
+<td>Last Name</td>
+<td>Active</td>
+</th></thead><tbody>";
+
+foreach ($arr as $line) {
+    echo "<tr>
+<td>$line[0]</td>
+<td>$line[1]</td>
+<td>$line[2]</td>
+<td>$line[3]</td>
+<td>$line[4]</td>
+<td>$line[5]</td>
+</tr>";
+}
+echo "</tbody> </table>";
+
+function getDB($q){
+    $arr = 1;
+
+    return $arr;
+}
+
+?>
